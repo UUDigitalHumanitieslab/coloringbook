c4caa25759d26d01f49702cc55c6cf2e71d71eaf
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-06-12 17:25:55 +0200

    Add simple Subject model per Flask-SQLAlchemy

--- coloringbook.py
+++ coloringbook.py
@@ -1,5 +1,25 @@
 from flask import Flask, render_template, request, json
+from flask.ext.sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://coloringbook@localhost/coloringbook'
+db = SQLAlchemy(app)
+
+class Subject (db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
+    numeral = db.Column(db.Integer)
+    birth = db.Column(db.DateTime)
+    eyesight = db.Column(db.String(100))
+    
+    languages = db.relationship(
+        'Language',
+        secondary = 'lang_bindings',
+        backref = db.backref('subjects')
+    )
+    
+    def __repr__ (self):
+        return '<Subject {0} born {1}>'.format(self.name, self.birth_date)
 
 @app.route('/')
 def index():
