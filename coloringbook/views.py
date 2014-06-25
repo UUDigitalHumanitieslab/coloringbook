33f17c6cb172b161834b31837bc2cad3ed434540
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-06-25 10:34:08 +0200

    Unpack list argument to 'date' in subject_from_json

--- coloringbook/views.py
+++ coloringbook/views.py
@@ -39,7 +39,7 @@ def subject_from_json (data):
     subject = Subject(
         name = data['name'],
         numeral = data['numeral'],  # TODO: rename on client side
-        birth = date(data['birth'].split('-')),  # TODO: enforce format
+        birth = date(*data['birth'].split('-')),  # TODO: enforce format
         eyesight = data['eyesight'] )
     
     for name, level in data['languages']:
