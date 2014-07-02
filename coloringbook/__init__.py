7d74f7da405a91fe8e4b31352baf9c2decf9727d
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-07-02 18:09:50 +0200

    Restructure application initialization to avoid context issues

--- coloringbook/__init__.py
+++ coloringbook/__init__.py
@@ -2,12 +2,18 @@ from flask import Flask
 
 from .models import db
 from .views import site
-from .admin import admin
+from .admin import create_admin
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://coloringbook@localhost/coloringbook'
+def create_app ( ):
+    app = Flask(__name__.split('.')[0])
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://coloringbook@localhost/coloringbook'
 
-db.init_app(app)
-admin.init_app(app)
+    db.init_app(app)
+    with app.app_context():
+        from .models import *
+        db.create_all(app = app)  # pass app because of Flask-SQLAlchemy contexts
+        admin = create_admin()
+        admin.init_app(app)
+    app.register_blueprint(site)
 
-app.register_blueprint(site)
+    return app
