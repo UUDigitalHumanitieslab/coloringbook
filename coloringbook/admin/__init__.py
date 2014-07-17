df0eda83dd713d1bef60ad584359af4d89ded807
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-07-17 11:04:11 +0200

    Add preliminary admin views

--- coloringbook/admin/__init__.py
+++ coloringbook/admin/__init__.py
@@ -1,10 +1,15 @@
 from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
 
-from ..models import db
+from ..models import db, Survey, Page, Color
 
 from .views import FillView
 
 def create_admin ( ):
+    sess = db.session
     admin = Admin(name='Coloringbook')
-    admin.add_view(FillView(db.session))
+    admin.add_view(ModelView(Survey, sess, name = 'Surveys'))
+    admin.add_view(ModelView(Page, sess, name = 'Pages'))
+    admin.add_view(ModelView(Color, sess, name = 'Colors'))
+    admin.add_view(FillView(sess))
     return admin
