41e7deca27bdb0759d7fe7f8e7dc0aa7a24efd51
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-07-23 15:09:10 +0200

    Add necessary hacks to retrieve options from the Page model

--- coloringbook/admin/__init__.py
+++ coloringbook/admin/__init__.py
@@ -6,11 +6,12 @@ from flask.ext.admin.contrib.fileadmin import FileAdmin
 
 from ..models import db, Page, Color, Language
 
-from .views import SurveyView, FillView
 
 def create_admin (app):
     sess = db.session
     admin = Admin(name='Coloringbook', app=app)
+    with app.app_context():
+        from .views import SurveyView, FillView
     admin.add_view(FillView(sess))
     admin.add_view(SurveyView(sess))
     admin.add_view(ModelView(Page, sess, name = 'Pages'))
