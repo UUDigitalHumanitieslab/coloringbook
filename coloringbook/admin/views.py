d58b974dfdfa55b00a1a8229369c8854c5d637a9
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-07-31 14:16:29 +0200

    Insert request method check for prefill hook as suggested by mrjoes

--- coloringbook/admin/views.py
+++ coloringbook/admin/views.py
@@ -43,7 +43,8 @@ class ModelView (sqla.ModelView):
                 else:
                     return redirect(return_url)
 
-        self.on_form_prefill(form, id)
+        if request.method == 'GET':
+            self.on_form_prefill(form, id)
         
         form_opts = FormOpts(widget_args=self.form_widget_args,
                              form_rules=self._form_edit_rules)
