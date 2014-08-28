9c0d7a5835daf8a2fc16f622c10444b4a413cf32
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-08-28 15:47:03 +0200

    Add automated test runner

---
+++ test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+# (c) 2014 Digital Humanities Lab, Faculty of Humanities, Utrecht University
+# Author: Julian Gonggrijp, j.gonggrijp@uu.nl
+
+"""
+    Script for automatically running the doctest test cases.
+"""
+
+from doctest import testmod, ELLIPSIS
+
+import coloringbook
+
+def test_all ( ):
+    testmod(coloringbook)
+    testmod(coloringbook.models)
+    testmod(coloringbook.views, optionflags = ELLIPSIS)
+    testmod(coloringbook.admin)
+    testmod(coloringbook.admin.utilities)
+    testmod(coloringbook.admin.forms)
+    testmod(coloringbook.admin.views)
+
+if __name__ == '__main__':
+    test_all()
