c2dd168178c5dc0b1b6d48fcad3eefb27143c06d
Julian Gonggrijp <j.gonggrijp@uu.nl>
2014-09-25 16:52:20 +0200

    Explicitly set InnoDB as the engine for all tables

--- coloringbook/models.py
+++ coloringbook/models.py
@@ -28,7 +28,58 @@ __all__ = [
     'SurveyPage',
     'Fill'              ]
 
-db = fsqla.SQLAlchemy()  # actual database connection is done in __init__.py
+def TableArgsMeta(parent_class, table_args):
+    """
+        Metaclass generator to set global defaults for __table_args__.
+        
+        See
+        http://stackoverflow.com/questions/25770701/how-to-tell-sqlalchemy-once-that-i-want-innodb-for-the-entire-database
+        for an explanation.
+    """
+
+    class _TableArgsMeta(parent_class):
+
+        def __init__(cls, name, bases, dict_):
+            if (    # Do not extend base class
+                    '_decl_class_registry' not in cls.__dict__ and 
+                    # Missing __tablename_ or equal to None means single table
+                    # inheritance -- no table for it (columns go to table of
+                    # base class)
+                    cls.__dict__.get('__tablename__') and
+                    # Abstract class -- no table for it (columns go to table[s]
+                    # of subclass[es]
+                    not cls.__dict__.get('__abstract__', False)):
+                ta = getattr(cls, '__table_args__', {})
+                if isinstance(ta, dict):
+                    ta = dict(table_args, **ta)
+                    cls.__table_args__ = ta
+                else:
+                    assert isinstance(ta, tuple)
+                    if ta and isinstance(ta[-1], dict):
+                        tad = dict(table_args, **ta[-1])
+                        ta = ta[:-1]
+                    else:
+                        tad = dict(table_args)
+                    cls.__table_args__ = ta + (tad,)
+            super(_TableArgsMeta, cls).__init__(name, bases, dict_)
+
+    return _TableArgsMeta
+
+class InnoDBSQLAlchemy (fsqla.SQLAlchemy):
+    """ Subclass in order to enable TableArgsMeta. """
+    def make_declarative_base(self):
+        """Creates the declarative base."""
+        base = fsqla.declarative_base(
+            cls = fsqla.Model,
+            name = 'Model',
+            mapper = fsqla.signalling_mapper,
+            metaclass = TableArgsMeta(
+                fsqla._BoundDeclarativeMeta,
+                {'mysql_engine': 'InnoDB'} ) )
+        base.query = fsqla._QueryProperty(self)
+        return base
+
+db = InnoDBSQLAlchemy()  # actual database connection is done in __init__.py
 
 class Subject (db.Model):
     """ Personal information of a test person. """
